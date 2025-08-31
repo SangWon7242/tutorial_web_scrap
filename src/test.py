510e48312cd59ea23a63f034de06aa437b31fefa
SangWon7242 <gotkdqja@gmail.com>
2025-08-31 14:50:42 +0900

    수집에 뉴스 제목을 엑셀에 저장

--- src/test.py
+++ src/test.py
@@ -1,6 +1,8 @@
 # 1. 라이브러리 불러오기
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import openpyxl
 
 # 2. 스크래핑할 URL 지정
 url = "https://news.naver.com/section/105"
@@ -33,10 +35,29 @@ if resp.status_code == 200:
   head_line_news_title_list = soup.select(".sa_text_strong")
   # print(head_line_news_title_list)
   
+  news_data_list = []
   for idx, title in enumerate(head_line_news_title_list):
     no = idx + 1
     print(f"{no} : {title.text}")
+    news_data_list.append(title.text)  
+    
+   # pandas를 사용하여 데이터를 엑셀 파일로 저장
+  if news_data_list: # 수집된 데이터가 있을 경우에만 실행
+    # 리스트를 pandas DataFrame으로 변환
+    # DataFrame은 엑셀 시트와 같은 표 형태의 데이터 구조
+    df = pd.DataFrame(news_data_list)
+
+    # DataFrame을 엑셀 파일로 저장
+    # index=False 옵션: 엑셀 파일에 불필요한 인덱스(0, 1, 2...) 열이 생기는 것을 방지
+    excel_filename = 'news_data.xlsx'
+    
+    save_path = f"D:\works\python_projects\scrap_data\{excel_filename}"
+    
+    df.to_excel(save_path, index=False, engine="openpyxl")
+    
+    print(f"'{excel_filename}' 파일로 저장이 완료되었습니다.")
+  else:
+      print("수집된 데이터가 없습니다.")   
     
 else:
   print(f"오류가 발생했습니다. 상태 코드: {resp.status_code}")
-
