96680f4bd55ed7e56e42d84798078fd2d22e644f
SangWon7242 <gotkdqja@gmail.com>
2025-08-31 14:40:34 +0900

    BeautifulSoup사용법, 네이버 뉴스 스크랩

--- src/test.py
+++ src/test.py
@@ -3,16 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 
 # 2. 스크래핑할 URL 지정
-url = "https://naver.com"
+url = "https://news.naver.com/section/105"
 
 # 3. requests를 사용해 웹페이지의 HTML 정보 요청
 resp = requests.get(url)
 
 # 4. 응답이 성공적인지 확인 (상태 코드 200은 성공을 의미)
 if resp.status_code == 200:      
-  print("요청 성공")
-  print(resp.url)
-  print(resp.text) # HTML 문서를 가져옴
+  # print("요청 성공")
+  # print(resp.url)
+  # print(resp.text) # HTML 문서를 가져옴
+  
+  # 5. BeautifulSoup을 사용해 HTML 코드를 파싱 가능한 객체로 변환
+  # 'lxml' 파서를 사용하도록 지정
+  soup = BeautifulSoup(resp.text, 'lxml')
+  # print(soup)
+  
+  # print(soup.find_all('strong', class_="sa_text_strong"))
+  
+  head_line_news_title_list = soup.find_all('strong', class_="sa_text_strong")
+  
+  for idx, title in enumerate(head_line_news_title_list):
+    no = idx + 1
+    print(f"{no} : {title.text}")
+    
 else:
   print(f"오류가 발생했습니다. 상태 코드: {resp.status_code}")
 
